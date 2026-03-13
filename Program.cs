94ee885655c96c2ee213c83a8ee29149e42e9cc4
ucenik <ucenik@DESKTOP-M7J8SQ9>
2026-03-13 17:34:38 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poligon2026_3_10_a
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aleksa Jankovic//
+           
+        }
+    }
+}
